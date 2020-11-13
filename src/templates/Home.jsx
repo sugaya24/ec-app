89328587c9b521bbfe9563154093af93a86c0898
YukiSugaya <sugaya324@gmail.com>
2020-11-12 16:58:17 -0800

    ADD: Install selector

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { getUserId, getUserName } from '../reducks/users/selectors';
+import { useSelector } from 'react-redux';
 
 export const Home = () => {
-  return <div>Home</div>;
+  const selector = useSelector((state) => state);
+  const uid = getUserId(selector);
+  const userName = getUserName(selector);
+
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>{uid}</p>
+      <p>{userName}</p>
+    </div>
+  );
 };
 
 export default Home;
--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { push } from 'connected-react-router';
+import { signInAction } from '../reducks/users/actions';
 
 export const Login = () => {
   const dispatch = useDispatch();
-  const selector = useSelector((state) => state);
-
-  console.log(selector.router);
 
   return (
     <div>
       <h2>Login</h2>
-      <button onClick={() => dispatch(push('/'))}>Login</button>
+      <button
+        onClick={() => {
+          dispatch(signInAction({ uid: '00001', username: 'yuki' }));
+          dispatch(push('/'));
+        }}
+      >
+        Login
+      </button>
     </div>
   );
 };
